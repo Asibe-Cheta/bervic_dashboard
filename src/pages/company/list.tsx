1c99ba2b498e199efa0379141395d93bea078fdc
Asibe Cheta <116509985+Asibe-Cheta@users.noreply.github.com>
2024-05-14 12:14:06 +0100

    fix errors

--- src/pages/company/list.tsx
+++ src/pages/company/list.tsx
@@ -1,132 +1,133 @@
 import CustomAvatar from '@/components/custom-avatar';
 import { Text } from '@/components/text';
 import { COMPANIES_LIST_QUERY } from '@/graphql/queries';
-import { Company } from '@/graphql/schema.types';
 import { CompaniesListQuery } from '@/graphql/types';
 import { currencyNumber } from '@/utilities';
 import { SearchOutlined } from '@ant-design/icons';
-import { CreateButton, DeleteButton, EditButton, FilterDropdown,
-   List, useTable } from '@refinedev/antd'
-   import { HttpError, getDefaultFilter, useGo } from '@refinedev/core';
+import { CreateButton, DeleteButton, EditButton, FilterDropdown, List, useTable } from '@refinedev/antd';
+import { HttpError, getDefaultFilter, useGo } from '@refinedev/core';
 import { GetFieldsFromList } from '@refinedev/nestjs-query';
 import { Input, Space, Table } from 'antd';
 
-export const CompanyList = ({children}: React.PropsWithChildren) => {
+interface Sum {
+  value: number;
+}
+
+interface DealAggregate {
+  sum: Sum;
+}
+
+interface Company {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+  avatarUrl?: string;
+  dealsAggregate?: DealAggregate[];
+}
+
+export const CompanyList = ({ children }: React.PropsWithChildren) => {
   const go = useGo();
-  const {tableProps, filters } = useTable< 
-  GetFieldsFromList< CompaniesListQuery>,
-  HttpError,
-  GetFieldsFromList<CompaniesListQuery>
-  > ({
+  const { tableProps, filters } = useTable<
+    GetFieldsFromList<CompaniesListQuery>,
+    HttpError,
+    GetFieldsFromList<CompaniesListQuery>
+  >({
     resource: 'companies',
     onSearch: (values) => {
       return [
         {
           field: 'name',
           operator: 'contains',
-          value: values.name
-        }
-      ]
+          value: values.name,
+        },
+      ];
     },
-    pagination:{
+    pagination: {
       pageSize: 12,
     },
     sorters: {
       initial: [
         {
-          field: 'createdAt',  
-          order: 'desc'
-        }
-      ]
+          field: 'createdAt',
+          order: 'desc',
+        },
+      ],
     },
-    
     filters: {
       initial: [
         {
           field: 'name',
           operator: 'contains',
-          value: undefined
-        }
-      ]
+          value: undefined,
+        },
+      ],
     },
     meta: {
-      gqlQuery: COMPANIES_LIST_QUERY
-    }
-  })
+      gqlQuery: COMPANIES_LIST_QUERY,
+    },
+  });
+
   return (
     <div>
-    <List
-    breadcrumb={false}
-    headerButtons={() => (
-      <CreateButton 
-      onClick={() => {
-        go({
-          to: {
-            resource:'companies',
-            action: 'create'
-          },
-          options: {
-            keepQuery: true
-          },
-          type: 'replace'
-        })
-      }}/>
-    )}
-    >
-      <Table
-      {...tableProps}
-      pagination={{
-        ...tableProps.pagination,
-      }}
+      <List
+        breadcrumb={false}
+        headerButtons={() => (
+          <CreateButton
+            onClick={() => {
+              go({
+                to: {
+                  resource: 'companies',
+                  action: 'create',
+                },
+                options: {
+                  keepQuery: true,
+                },
+                type: 'replace',
+              });
+            }}
+          />
+        )}
       >
-      <Table.Column<Company> 
-      dataIndex ="name"
-      title = "Company Title"
-      defaultFilteredValue = { getDefaultFilter('id', filters)}
-      filterIcon={<SearchOutlined />}
-      filterDropdown={(props) => (
-        <FilterDropdown{...props}>
-        <Input placeholder= "Search Company" />
-        </FilterDropdown>
-      )}
-      render={(value, record) => (
-        <Space>
-          <CustomAvatar shape="square" 
-          name={record.name} src={record.avatarUrl} />
-          <Text style={{ whiteSpace: 'nowrap'}}>
-            {record.name}
-          </Text>
-        </Space>
-      )} 
-      />
-      <Table.Column<Company>
-        dataIndex ="totalRevenue"
-        title="Open deals amount"
-        render={(value, company) => (
-          <Text>
-            {currencyNumber(company?.dealsAggregate?.[0].sum?. 
-              value || 0)}
-          </Text>
-        )}
-      
-        />
-        <Table.Column<Company>
-        dataIndex="id"
-        title="Actions"
-        fixed="right"
-        render={(value) =>(
-         <Space>
-          <EditButton hideText size="small" recordItemId={value}/>
-          <DeleteButton hideText size="small" recordItemId={value}/>
-         </Space>
-        )}
-      
-      />
-
-      </Table>
-
-    </List>
-    {children}
+        <Table {...tableProps} pagination={{ ...tableProps.pagination }}>
+          <Table.Column<Company>
+            dataIndex="name"
+            title="Company Title"
+            defaultFilteredValue={getDefaultFilter('id', filters)}
+            filterIcon={<SearchOutlined />}
+            filterDropdown={(props) => (
+              <FilterDropdown {...props}>
+                <Input placeholder="Search Company" />
+              </FilterDropdown>
+            )}
+            render={(value, record) => (
+              <Space>
+                <CustomAvatar shape="square" name={record.name} src={record.avatarUrl} />
+                <Text style={{ whiteSpace: 'nowrap' }}>{record.name}</Text>
+              </Space>
+            )}
+          />
+          <Table.Column<Company>
+            dataIndex="totalRevenue"
+            title="Open deals amount"
+            render={(value, company) => (
+              <Text>{currencyNumber(company?.dealsAggregate?.[0]?.sum?.value || 0)}</Text>
+            )}
+          />
+          <Table.Column<Company>
+            dataIndex="id"
+            title="Actions"
+            fixed="right"
+            render={(value) => (
+              <Space>
+                <EditButton hideText size="small" recordItemId={value} />
+                <DeleteButton hideText size="small" recordItemId={value} />
+              </Space>
+            )}
+          />
+        </Table>
+      </List>
+      {children}
     </div>
-  )
-}
+  );
+};
